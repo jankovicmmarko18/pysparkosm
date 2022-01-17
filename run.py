a89d7957e3cf6ede646ced117c95e692a995612e
Marko Jankovic <66789574+jankovicmmarko18@users.noreply.github.com>
2022-01-17 18:02:43 +0100

    refactor1

--- run.py
+++ run.py
@@ -3,44 +3,7 @@ from functions import *
 #CHANGE pbfname to match the available pbf source name (see available sources by calling sources.(arguments) see below)
 directory='../osm'
 pbfnames=['chile','argentina','ecuador']
-
-for pbfname in pbfnames:
-    print('PBF NAME: ',pbfname,'\n','###################################')
-
-    try:
-        os.mkdir(directory+'/'+pbfname)
-        os.mkdir(directory+'/'+pbfname+'/nodes')
-        os.mkdir(directory+'/'+pbfname+'/ways')
-        os.mkdir(directory+'/'+pbfname+'/relations')
-        os.mkdir(directory+'/'+pbfname+'/combination')
-    except:
-        print('some of the directories already exist')
-
-
-    #UNCOMENT TO SEE AVAILABLE SOURCES
-    #sources.available.keys()
-    #sources.south_america.available
-    #sources.subregions.available.keys()
-    #sources.subregions.brazil.available
-
-    #PYROSM GET FILES
-    print('getting data from osm')
-    fp=get_data(pbfname,directory=directory+'/'+pbfname,update=False)
-    print('data downloaded')
-
-    #PARQUETIZE PBF FILES TO NODE, WAY, RELATION
-    driver_location='/home/marko/osm-parquetizer/target/osm-parquetizer-1.0.1-SNAPSHOT.jar'
-    print('start parquetizing')
-    parquetize(driver_location,directory+'/'+pbfname+'/'+pbfname+'-latest.osm.pbf')
-    print('parquetize ended')
-
-    node=spark.read.parquet(directory+'/'+pbfname+'/'+pbfname+'-latest.osm.pbf.node.parquet')
-    way=spark.read.parquet(directory+'/'+pbfname+'/'+pbfname+'-latest.osm.pbf.way.parquet')
-    relation=spark.read.parquet(directory+'/'+pbfname+'/'+pbfname+'-latest.osm.pbf.relation.parquet')
-
-    #RUN SCRIPTS:
-
-    filterr={
+filterr={
     'amenity':[
         'college','driving_school','kindergarten',
         'language_school','library','toy_library',
@@ -66,38 +29,69 @@ for pbfname in pbfnames:
     'history':'',
     'military':'',
     'power':''
-
         }
 
-    #poi_extractor(file=node,filterr=filterr,save_geojson=True,pbfname=pbfname,directory=directory+'/'+pbfname+'/nodes')
+kwargs={'engine':pg_connection('marko','rumarec18','34.147.9.88','5432','crowdpulse')[0],
+       #'conn':pg_connection(username,password,host,port,dbname)[1],
+        'geoserver_url':'http://34.147.9.88:8080/geoserver',
+        'geoserver_username':'admin',
+        'geoserver_pass':'Rumarec18*',
+        'geoserver_wspace':'crowdpulse',
+        'geoserver_store':'crowdpulse_db_polygons'
+       }
 
 
-    kwargs={'engine':pg_connection('marko','rumarec18','34.147.9.88','5432','crowdpulse')[0],
-           #'conn':pg_connection(username,password,host,port,dbname)[1],
-            'geoserver_url':'http://34.147.9.88:8080/geoserver',
-            'geoserver_username':'admin',
-            'geoserver_pass':'Rumarec18*',
-            'geoserver_wspace':'crowdpulse',
-            'geoserver_store':'crowdpulse_db_polygons'
-           }
+for pbfname in pbfnames:
+    print('PBF NAME: ',pbfname,'\n','###################################')
+
+    try:
+        os.mkdir(directory+'/embassies')
+        os.mkdir(directory+'/'+pbfname)
+        os.mkdir(directory+'/'+pbfname+'/nodes')
+        os.mkdir(directory+'/'+pbfname+'/ways')
+        os.mkdir(directory+'/'+pbfname+'/relations')
+        os.mkdir(directory+'/'+pbfname+'/combination')
+    except:
+        print('some of the directories already exist')
 
 
+    #UNCOMENT TO SEE AVAILABLE SOURCES
+    #sources.available.keys()
+    #sources.south_america.available
+    #sources.subregions.available.keys()
+    #sources.subregions.brazil.available
+
+    #PYROSM GET FILES
+    print('getting data from osm')
+    fp=get_data(pbfname,directory=directory+'/'+pbfname,update=True)
+    print('data downloaded')
+
+    #PARQUETIZE PBF FILES TO NODE, WAY, RELATION
+    driver_location='/home/marko/osm-parquetizer/target/osm-parquetizer-1.0.1-SNAPSHOT.jar'
+    print('start parquetizing')
+    parquetize(driver_location,directory+'/'+pbfname+'/'+pbfname+'-latest.osm.pbf')
+    print('parquetize ended')
+
+    #READ PARQUET FILES
+    node=spark.read.parquet(directory+'/'+pbfname+'/'+pbfname+'-latest.osm.pbf.node.parquet')
+    way=spark.read.parquet(directory+'/'+pbfname+'/'+pbfname+'-latest.osm.pbf.way.parquet')
+    relation=spark.read.parquet(directory+'/'+pbfname+'/'+pbfname+'-latest.osm.pbf.relation.parquet')
+
+    #RUN SCRIPTS:
+    
+    
     #WAYS:
     pdfs=pull_buildings(node,way)
     fname=directory+'/'+pbfname+'/'+'ways/'+pbfname+'-latest-building'
     dump_buildings_to_geojson(fname,pdfs)
 
     #RELATIONS:
-
     pdf=pull_buildings_relations(node,way,relation)
-
-    #fname_rl MUST CONTAIN relation WORD IN ORDER TO BE RECOGNIZED AND SEPARATED IN SPECIFIC TABLE BECAUSE OF DIFFERENT GEOMETRY TYPE
     fname_rl=directory+'/'+pbfname+'/'+'relations/'+pbfname+'-latest-building-relations'
     dump_buildings_to_geojson_relation(fname_rl,pdf)
 
 
     #READ AND PUSH POLYGONS TO DB AND PUBLISH TO GEOSERVER
-
     read_gpd(path=directory+'/'+pbfname,table_name=pbfname,scheema='polygons',**kwargs)
 
 
